6c7f7653489409ce3983cc14198ece707e67203f
Ania <annalangner06@gmail.com>
2019-06-05 08:32:16 +0200

    The first attepmt to change the code

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-class Stopwatch {
-    constructor(display) {
-        this.running = false;
-        this.display = display;
-        this.reset();
-        this.print(this.times);
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
     }
+    return result;
+}
 
-    reset() {
-        this.times = {
+class Stopwatch extends React.Component{
+    className;
+    constructor(display) {
+        super(display);
+        this.state = {
+            running: false,
             minutes: 0,
             seconds: 0,
             miliseconds: 0
         };
+        this.print(this.times);
     }
 
     print() {
         this.display.innerText = this.format(this.times);
-    }
+    };
 
-    format(times) {
+    static format(times) {
         return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
-    }
-
+    };
 
     start() {
         if (!this.running) {
             this.running = true;
             this.watch = setInterval(() => this.step(), 10);
         }
-    }
+    };
 
     step() {
         if(!this.running) return;
         this.calculate();
         this.print();
-    }
+    };
 
     calculate() {
         this.times.miliseconds += 1;
@@ -46,26 +50,31 @@ class Stopwatch {
             this.times.minutes += 1;
             this.times.seconds = 0;
         }
-    }
+    };
 
     stop() {
-        this.running = false;
+        this.setState({
+            running: false
+        });
         clearInterval(this.watch);
-    }
+    };
+
+    render() {
+        return (
+            <div className={'app'} id={'app'}>
+                <button onClick={this.start}>Start</button>
+                <button onClick={this.stop}>Stop</button>
+                <p className={'stopwatch'}>
+                    {this.format({
+                        minutes: this.times.minutes,
+                        seconds: this.times.seconds,
+                        miliseconds: this.times.miliseconds
+                    })}
+                </p>
+            </div>
+        )
+    };
 }
 
-function pad0(value) {
-    let result = value.toString();
-    if (result.length < 2) {
-        result = '0' + result;
-    }
-    return result;
-}
-
-const stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
-
-let startButton = document.getElementById('start');
-startButton.addEventListener('click', () => stopwatch.start());
-
-let stopButton = document.getElementById('stop');
-stopButton.addEventListener('click', () => stopwatch.stop());
+const stopwatch = React.createElement(Stopwatch);
+ReactDOM.render(stopwatch, document.getElementById('app'));
